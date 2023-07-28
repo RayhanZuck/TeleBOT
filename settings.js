9c41935f15a9a8195ed63860f95923f24b6d8d79
Rayhan Zuckerberg <76443273+RayhanZuck@users.noreply.github.com>
2023-07-28 15:04:23 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const {
+   indonesia
+} = require("./language");
+
+// Website Api (jgn di ganti biar gk eror)
+global.APIs = {
+   alfa: 'https://api.zeeoneofc.my.id', //apabila link api eror, segera laporkan ke owner
+}
+
+//buy apikey premium 0887435047326
+// Free apikey (silahkan login terus ganti Your Key dgn apikey lu)
+global.APIKeys = {
+   'https://api.zeeoneofc.my.id': '2njIzfIJd3wRKYi', // 👉 login https://api.zeeoneofc.my.id to get apikey
+}
+
+//language 
+global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+
+global.BOT_TOKEN = "6013009643:AAHpCpoMOyVz3l8aPtKxld8jvRQtaAtNUac" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "RayBOT" //your bot name
+global.OWNER_NAME = "RayhanXD." //your name
+global.OWNER_NUMBER = "087751753538" //your telegram number
+global.OWNER = ["https://t.me/RayhanXD"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.lang = language //don't change
